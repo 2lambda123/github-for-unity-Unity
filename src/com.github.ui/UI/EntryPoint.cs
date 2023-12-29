2a392c52d9cde9ceb51cdf12390eb2220e87c87c
Restyled.io <commits@restyled.io>
2023-12-29 09:20:13 +0000

    Restyled by clang-format

--- src/com.github.ui/UI/EntryPoint.cs
+++ src/com.github.ui/UI/EntryPoint.cs
@@ -4,96 +4,91 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-namespace GitHub.Unity
-{
+namespace GitHub.Unity {
 [InitializeOnLoad]
-public class EntryPoint : ScriptableObject
-{
-    // this may run on the loader thread if it's an appdomain restart
-    static EntryPoint()
-    {
-        var tempEnv = new DefaultEnvironment();
-        if (tempEnv.GetEnvironmentVariable("GITHUB_UNITY_DISABLE") == "1")
-        {
-            Debug.Log("GitHub for Unity " + ApplicationInfo.Version + " is disabled");
-            return;
-        }
-
-        LogHelper.LogAdapter = new FileLogAdapter(tempEnv.LogPath);
-
-        EditorApplication.update += Initialize;
+public class EntryPoint : ScriptableObject {
+  // this may run on the loader thread if it's an appdomain restart
+  static EntryPoint() {
+    var tempEnv = new DefaultEnvironment();
+    if (tempEnv.GetEnvironmentVariable("GITHUB_UNITY_DISABLE") == "1") {
+      Debug.Log("GitHub for Unity " + ApplicationInfo.Version + " is disabled");
+      return;
     }
 
-    // we do this so we're guaranteed to run on the main thread, not the loader thread
-    private static void Initialize()
-    {
-        EditorApplication.update -= Initialize;
+    LogHelper.LogAdapter = new FileLogAdapter(tempEnv.LogPath);
 
-        // this will initialize ApplicationManager and Environment if they haven't yet
-        var logPath = ApplicationManager.Environment.LogPath;
+    EditorApplication.update += Initialize;
+  }
 
-        if (ApplicationCache.Instance.FirstRun)
-        {
-            var oldLogPath = logPath.Parent.Combine(logPath.FileNameWithoutExtension + "-old" + logPath.ExtensionWithDot);
-            try
-            {
-                var shouldRotate = true;
+  // we do this so we're guaranteed to run on the main thread, not the loader
+  // thread
+  private static void Initialize() {
+    EditorApplication.update -= Initialize;
+
+    // this will initialize ApplicationManager and Environment if they haven't
+    // yet
+    var logPath = ApplicationManager.Environment.LogPath;
+
+    if (ApplicationCache.Instance.FirstRun) {
+      var oldLogPath = logPath.Parent.Combine(
+          logPath.FileNameWithoutExtension + "-old" + logPath.ExtensionWithDot);
+      try {
+        var shouldRotate = true;
 #if GFU_DEBUG_BUILD
-                shouldRotate = new FileInfo(logPath).Length > 10 * 1024 * 1024;
+        shouldRotate = new FileInfo(logPath).Length > 10 * 1024 * 1024;
 #endif
-                if (shouldRotate)
-                {
-                    oldLogPath.DeleteIfExists();
-                    if (logPath.FileExists())
-                    {
-                        logPath.Move(oldLogPath);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                LogHelper.Error(ex, "Error rotating log files");
-            }
-
-            Debug.LogFormat("Initialized Git for Unity version {0}{1} ({2}) Log file: {2}", ApplicationInfo.Version, ApplicationManager.Environment.NewLine, logPath);
+        if (shouldRotate) {
+          oldLogPath.DeleteIfExists();
+          if (logPath.FileExists()) {
+            logPath.Move(oldLogPath);
+          }
         }
+      } catch (Exception ex) {
+        LogHelper.Error(ex, "Error rotating log files");
+      }
 
-        LogHelper.LogAdapter = new MultipleLogAdapter(new FileLogAdapter(logPath)
+      Debug.LogFormat(
+          "Initialized Git for Unity version {0}{1} ({2}) Log file: {2}",
+          ApplicationInfo.Version, ApplicationManager.Environment.NewLine,
+          logPath);
+    }
+
+    LogHelper.LogAdapter = new MultipleLogAdapter(new FileLogAdapter(logPath)
 #if GFU_DEBUG_BUILD
-                , new UnityLogAdapter()
+                                                      ,
+                                                  new UnityLogAdapter()
 #endif
-                                                     );
+    );
 
-        LogHelper.Info("Initializing GitForUnity:'v{0}' Unity:'v{1}'", ApplicationInfo.Version, ApplicationManager.Environment.UnityVersion);
+    LogHelper.Info("Initializing GitForUnity:'v{0}' Unity:'v{1}'",
+                   ApplicationInfo.Version,
+                   ApplicationManager.Environment.UnityVersion);
 
-        ApplicationManager.Run();
+    ApplicationManager.Run();
 
-        if (ApplicationCache.Instance.FirstRun)
-            UpdateCheckWindow.CheckForUpdates(ApplicationManager);
+    if (ApplicationCache.Instance.FirstRun)
+      UpdateCheckWindow.CheckForUpdates(ApplicationManager);
+  }
+
+  internal static void Restart() {
+    if (appManager != null) {
+      appManager.Dispose();
+      appManager = null;
     }
 
-    internal static void Restart()
-    {
-        if (appManager != null)
-        {
-            appManager.Dispose();
-            appManager = null;
-        }
+    Initialize();
+  }
 
-        Initialize();
+  private static ApplicationManager appManager;
+  public static IApplicationManager ApplicationManager {
+    get {
+      if (appManager == null) {
+        appManager =
+            new ApplicationManager(new MainThreadSynchronizationContext(),
+                                   EnvironmentCache.Instance.Environment);
+      }
+      return appManager;
     }
-
-    private static ApplicationManager appManager;
-    public static IApplicationManager ApplicationManager
-    {
-        get
-        {
-            if (appManager == null)
-            {
-                appManager = new ApplicationManager(new MainThreadSynchronizationContext(), EnvironmentCache.Instance.Environment);
-            }
-            return appManager;
-        }
-    }
+  }
 }
 }
--- src/com.github.ui/UI/UI/InitProjectView.cs
+++ src/com.github.ui/UI/UI/InitProjectView.cs
@@ -3,124 +3,117 @@ using Unity.VersionControl.Git;
 using UnityEditor;
 using UnityEngine;
 
-namespace GitHub.Unity
-{
+namespace GitHub.Unity {
 [Serializable]
-class InitProjectView : Subview
-{
-    private const string NoRepoTitle = "To begin using GitHub, initialize a git repository";
-    private const string NoUserOrEmailError = "Name and email not set in git. Go into the settings tab and enter the missing information";
+class InitProjectView : Subview {
+  private const string NoRepoTitle =
+      "To begin using GitHub, initialize a git repository";
+  private const string NoUserOrEmailError =
+      "Name and email not set in git. Go into the settings tab and enter the missing information";
 
-    [SerializeField] private bool hasCompletedInitialCheck;
-    [SerializeField] private bool isUserDataPresent;
+  [SerializeField]
+  private bool hasCompletedInitialCheck;
+  [SerializeField]
+  private bool isUserDataPresent;
 
-    [SerializeField] private CacheUpdateEvent lastCheckUserChangedEvent;
+  [SerializeField]
+  private CacheUpdateEvent lastCheckUserChangedEvent;
 
-    [NonSerialized] private bool userHasChanges;
-    [NonSerialized] private bool gitExecutableIsSet;
+  [NonSerialized]
+  private bool userHasChanges;
+  [NonSerialized]
+  private bool gitExecutableIsSet;
 
-    public override void InitializeView(IView parent)
+  public override void InitializeView(IView parent) {
+    base.InitializeView(parent);
+    gitExecutableIsSet = Environment.GitExecutablePath.IsInitialized;
+    Redraw();
+  }
+
+  public override void OnEnable() {
+    base.OnEnable();
+    AttachHandlers();
+
+    User.CheckAndRaiseEventsIfCacheNewer(CacheType.GitUser,
+                                         lastCheckUserChangedEvent);
+  }
+
+  public override void OnDisable() {
+    base.OnDisable();
+    DetachHandlers();
+  }
+
+  public override void OnUI() {
+    GUILayout.BeginVertical(Styles.GenericBoxStyle);
     {
-        base.InitializeView(parent);
-        gitExecutableIsSet = Environment.GitExecutablePath.IsInitialized;
-        Redraw();
-    }
+      GUILayout.FlexibleSpace();
+      GUILayout.Space(-140);
 
-    public override void OnEnable()
-    {
-        base.OnEnable();
-        AttachHandlers();
+      GUILayout.BeginHorizontal();
+      {
+        GUILayout.FlexibleSpace();
+        GUILayout.Label(Styles.EmptyStateInit, GUILayout.MaxWidth(265),
+                        GUILayout.MaxHeight(136));
+        GUILayout.FlexibleSpace();
+      }
+      GUILayout.EndHorizontal();
 
-        User.CheckAndRaiseEventsIfCacheNewer(CacheType.GitUser, lastCheckUserChangedEvent);
-    }
+      GUILayout.Label(NoRepoTitle, Styles.BoldCenteredLabel);
+      GUILayout.Space(4);
 
-    public override void OnDisable()
-    {
-        base.OnDisable();
-        DetachHandlers();
-    }
+      GUILayout.BeginHorizontal();
+      {
+        GUILayout.FlexibleSpace();
 
-    public override void OnUI()
-    {
-        GUILayout.BeginVertical(Styles.GenericBoxStyle);
+        EditorGUI.BeginDisabledGroup(!gitExecutableIsSet || IsBusy ||
+                                     !isUserDataPresent);
         {
-            GUILayout.FlexibleSpace();
-            GUILayout.Space(-140);
-
-            GUILayout.BeginHorizontal();
-            {
-                GUILayout.FlexibleSpace();
-                GUILayout.Label(Styles.EmptyStateInit, GUILayout.MaxWidth(265), GUILayout.MaxHeight(136));
-                GUILayout.FlexibleSpace();
-            }
-            GUILayout.EndHorizontal();
-
-            GUILayout.Label(NoRepoTitle, Styles.BoldCenteredLabel);
-            GUILayout.Space(4);
-
-            GUILayout.BeginHorizontal();
-            {
-                GUILayout.FlexibleSpace();
-
-                EditorGUI.BeginDisabledGroup(!gitExecutableIsSet || IsBusy || !isUserDataPresent);
-                {
-                    if (GUILayout.Button(Localization.InitializeRepositoryButtonText, "Button"))
-                    {
-                        Manager.InitializeRepository();
-                    }
-                }
-                EditorGUI.EndDisabledGroup();
-
-                GUILayout.FlexibleSpace();
-            }
-            GUILayout.EndHorizontal();
-
-            if (gitExecutableIsSet && hasCompletedInitialCheck && !isUserDataPresent)
-            {
-                EditorGUILayout.Space();
-                EditorGUILayout.HelpBox(NoUserOrEmailError, MessageType.Error);
-            }
-
-            GUILayout.FlexibleSpace();
+          if (GUILayout.Button(Localization.InitializeRepositoryButtonText,
+                               "Button")) {
+            Manager.InitializeRepository();
+          }
         }
-        GUILayout.EndVertical();
-    }
+        EditorGUI.EndDisabledGroup();
 
-    public override void OnDataUpdate()
-    {
-        base.OnDataUpdate();
-        MaybeUpdateData();
-    }
+        GUILayout.FlexibleSpace();
+      }
+      GUILayout.EndHorizontal();
 
-    private void AttachHandlers()
-    {
-        User.Changed += UserOnChanged;
-    }
+      if (gitExecutableIsSet && hasCompletedInitialCheck &&
+          !isUserDataPresent) {
+        EditorGUILayout.Space();
+        EditorGUILayout.HelpBox(NoUserOrEmailError, MessageType.Error);
+      }
 
-    private void UserOnChanged(CacheUpdateEvent cacheUpdateEvent)
-    {
-        if (!lastCheckUserChangedEvent.Equals(cacheUpdateEvent))
-        {
-            lastCheckUserChangedEvent = cacheUpdateEvent;
-            userHasChanges = true;
-            Redraw();
-        }
+      GUILayout.FlexibleSpace();
     }
+    GUILayout.EndVertical();
+  }
 
-    private void DetachHandlers()
-    {
-        User.Changed -= UserOnChanged;
-    }
+  public override void OnDataUpdate() {
+    base.OnDataUpdate();
+    MaybeUpdateData();
+  }
 
-    private void MaybeUpdateData()
-    {
-        if (userHasChanges)
-        {
-            userHasChanges = false;
-            isUserDataPresent = !string.IsNullOrEmpty(User.Name)
-                                && !string.IsNullOrEmpty(User.Email);
-            hasCompletedInitialCheck = true;
-        }
+  private void AttachHandlers() { User.Changed += UserOnChanged; }
+
+  private void UserOnChanged(CacheUpdateEvent cacheUpdateEvent) {
+    if (!lastCheckUserChangedEvent.Equals(cacheUpdateEvent)) {
+      lastCheckUserChangedEvent = cacheUpdateEvent;
+      userHasChanges = true;
+      Redraw();
     }
+  }
+
+  private void DetachHandlers() { User.Changed -= UserOnChanged; }
+
+  private void MaybeUpdateData() {
+    if (userHasChanges) {
+      userHasChanges = false;
+      isUserDataPresent =
+          !string.IsNullOrEmpty(User.Name) && !string.IsNullOrEmpty(User.Email);
+      hasCompletedInitialCheck = true;
+    }
+  }
 }
 }
